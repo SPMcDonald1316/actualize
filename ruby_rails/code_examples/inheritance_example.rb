f78f957e9bf82f89c86afd55a7cbc588148269d8
sean <spmcdonald1316@gmail.com>
2022-03-09 11:08:51 -0600

    add module example for car and bike classes

--- ruby_rails/code_examples/inheritance_example.rb
+++ ruby_rails/code_examples/inheritance_example.rb
@@ -47,10 +47,43 @@
 #   end
 # end
 
-# SOLUTION
+# SOLUTION 1
 # Create a thrid class that encompasses the shared methods while leaving the differing methods in their respective classes
 
-class Vehicle
+# class Vehicle
+#   def initialize
+#     @speed = 0
+#     @direction = 'north'
+#   end
+
+#   def brake
+#     @speed = 0
+#   end
+
+#   def accelerate
+#     @speed += 10
+#   end
+
+#   def turn(new_direction)
+#     @direction = new_direction
+#   end
+# end
+
+# class Car < Vehicle
+#   def honk_horn
+#     puts "Beeeeep!"
+#   end
+# end
+
+# class Bike < Vehicle
+#   def ring_bell
+#     puts "Ring ring!"
+#   end
+# end
+
+# SOLUTION 2
+module Moveable
+  attr_reader :speed, :direction
   def initialize
     @speed = 0
     @direction = 'north'
@@ -69,14 +102,41 @@ class Vehicle
   end
 end
 
-class Car < Vehicle
+class Car
+  include Moveable
+
   def honk_horn
     puts "Beeeeep!"
   end
 end
 
-class Bike < Vehicle
+class Bike
+  include VehicleMoveable
+
   def ring_bell
     puts "Ring ring!"
   end
 end
+
+bike = Bike.new
+car = Car.new
+
+p bike.speed
+p bike.direction
+puts
+
+bike.accelerate
+bike.turn('south')
+p bike.speed
+p bike.direction
+
+puts
+p car.speed
+p car.direction
+puts
+
+car.accelerate
+car.accelerate
+car.turn('west')
+p car.speed
+p car.direction
