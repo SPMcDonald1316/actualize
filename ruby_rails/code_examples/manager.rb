2df0db2fdb81a3dc87ada5d4cebc92132ccac2dc
sean <spmcdonald1316@gmail.com>
2022-03-09 10:35:30 -0600

    add give_all_raises and fire_all_employees methods

--- ruby_rails/code_examples/manager.rb
+++ ruby_rails/code_examples/manager.rb
@@ -18,9 +18,36 @@ class Employee
   end
 end
 
-employee = Employee.new({first_name: 'Danilo', last_name: 'Campos', salary: 80000, active: true})
+employee1 = Employee.new(first_name: 'Danilo', last_name: 'Campos', salary: 80000, active: true)
+employee2 = Employee.new(first_name: 'Majora', last_name: 'Carter', salary: 70000, active: false)
 
 class Manager < Employee
+  attr_reader :employees
+  # To add employees instance variable to Manager class need to create its own initialize method with variable
+  # Code is no longer DRY since this initialize method will take precedence over any inherited initialize method
+  # This is solved by using super in Manager initialize method
+  # Super grabs all instance variables from inherited class first then creates added variables from new initialize
+  def initialize(input_options)
+    # @first_name = input_options[:first_name]
+    # @last_name = input_options[:last_name]
+    # @salary = input_options[:salary]
+    # @active = input_options[:active]
+    super
+    @employees = input_options[:employees]
+  end
+
+  def give_all_raises
+    employees.each do |employee|
+      employee.give_annual_raise
+    end
+  end
+
+  def fire_all_employees
+    employees.each do |employee|
+      employee.active = false
+    end
+  end
+
   def send_report
     p "going to send a report..."
     # do some logic here to send emails etc
@@ -28,8 +55,16 @@ class Manager < Employee
   end
 end
 
-manager1 = Manager.new(first_name: 'Manny', last_name: 'Williams', salary: 100000, active: true)
+manager1 = Manager.new(first_name: 'Manny', last_name: 'Williams', salary: 100000, active: true, employees: [employee1, employee2])
 
 manager1.print_info
 
 manager1.send_report
+
+p manager1.employees
+puts 
+manager1.give_all_raises
+p manager1.employees
+manager1.fire_all_employees
+puts
+p manager1.employees
