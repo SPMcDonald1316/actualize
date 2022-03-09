a368975fea6714d9c42b1e318b970047ab44d683
sean <spmcdonald1316@gmail.com>
2022-03-09 09:53:36 -0600

    add inheritance_example

---
+++ ruby_rails/code_examples/inheritance_example.rb
@@ -0,0 +1,82 @@
+# Two classes that share initialize, brake, accelerate, and turn methods
+# NOT DRY 
+# Problem is inheritance alone won't fix since we don't want Bike to inherit honk_horn or Car to inherit ring_bell
+
+# class Car
+#   def initialize
+#     @speed = 0
+#     @direction = 'north'
+#   end
+
+#   def brake
+#     @speed = 0
+#   end
+
+#   def accelerate
+#     @speed += 10
+#   end
+
+#   def turn(new_direction)
+#     @direction = new_direction
+#   end
+
+#   def honk_horn
+#     puts "Beeeeep!"
+#   end
+# end
+
+# class Bike
+#   def initialize
+#     @speed = 0
+#   end
+
+#   def brake
+#     @speed = 0
+#   end
+
+#   def accelerate
+#     @speed += 10
+#   end
+
+#   def turn(new_direction)
+#     @direction = new_direction
+#   end
+
+#   def ring_bell
+#     puts "Ring ring!"
+#   end
+# end
+
+# SOLUTION
+# Create a thrid class that encompasses the shared methods while leaving the differing methods in their respective classes
+
+class Vehicle
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car < Vehicle
+  def honk_horn
+    puts "Beeeeep!"
+  end
+end
+
+class Bike < Vehicle
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
