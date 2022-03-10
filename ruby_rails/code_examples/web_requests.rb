0d0d3152feaa98d33e84fd52aeabd8debdaabb7f
sean <spmcdonald1316@gmail.com>
2022-03-10 11:21:56 -0600

    create api get request based on user input

--- ruby_rails/code_examples/web_requests.rb
+++ ruby_rails/code_examples/web_requests.rb
@@ -1,5 +1,19 @@
 require 'http'
 
-response = HTTP.get("https://data.ny.gov/resource/xjya-f8ng.json")
+# Simple get request
+# response = HTTP.get("https://data.ny.gov/resource/xjya-f8ng.json")
 
-p response.parse
+# p response.parse
+
+# Basic weather app
+# user enters a city
+# show the weather in that city
+# display 'It is current #{weather} in #{city}'
+
+puts "Enter a city to see current weather."
+city = gets.chomp
+
+response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&units=imperial&appid=de73dde8c668aa452a3a73ac2cad74e2").parse
+
+
+# print ("It is #{response[:main][:temp]} degrees and #{response[:weather][0][:description]} in #{response[:name]}.")
