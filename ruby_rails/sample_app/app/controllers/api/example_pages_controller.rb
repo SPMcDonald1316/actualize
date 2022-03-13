21f4742cfc1167f4026ee9173883bc06505e298d
sean <spmcdonald1316@gmail.com>
2022-03-13 13:00:00 -0500

    create simple routes, render a message in controller

--- ruby_rails/sample_app/app/controllers/api/example_pages_controller.rb
+++ ruby_rails/sample_app/app/controllers/api/example_pages_controller.rb
@@ -1,2 +1,9 @@
 class Api::ExamplePagesController < ApplicationController
+  def welcome
+    render json: {message: "hello"}
+  end
+
+  def table
+    render json: {message: "That's a nice table!"}
+  end
 end
--- ruby_rails/sample_app/config/routes.rb
+++ ruby_rails/sample_app/config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+
+  namespace :api do
+    get "/hello" => "example_pages#welcome"
+
+    get "/table" => "example_pages#table"
+  end
 end
