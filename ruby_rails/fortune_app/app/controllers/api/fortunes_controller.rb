352beeeab4f7c42c182de0d248818f5cb6453ea2
sean <spmcdonald1316@gmail.com>
2022-03-16 11:09:00 -0500

    update lotto nums logic

--- ruby_rails/fortune_app/app/controllers/api/fortunes_controller.rb
+++ ruby_rails/fortune_app/app/controllers/api/fortunes_controller.rb
@@ -3,10 +3,9 @@ class Api::FortunesController < ApplicationController
     fortunes = ['You have been blessed', 'You have been cursed', 'No fortune']
     @fortune = fortunes[rand(3)]
 
-    lotto = Array.new(6)
-    lotto.map! {|x| x = rand(1..60)}
+    @lotto = Array.new(6)
+    @lotto.map! {|x| x = rand(1..60)}
     
-    @lotto = lotto
     render 'fortune.json.jb'
   end
 end
