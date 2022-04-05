c548e31b0143888f145e880947c414e2e576cbe1
sean <spmcdonald1316@gmail.com>
2022-04-05 10:14:34 -0500

    generate contact model

---
+++ ruby_rails/contacts_app/db/migrate/20220405151411_create_contacts.rb
@@ -0,0 +1,12 @@
+class CreateContacts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :contacts do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
