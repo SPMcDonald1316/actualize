6dc2125d927c39fb2c513fd273b60bae9d1e6714
sean <spmcdonald1316@gmail.com>
2022-04-12 10:54:04 -0500

    update database using seeds file

--- ruby_rails/contacts_app/db/seeds.rb
+++ ruby_rails/contacts_app/db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# contact = Contact.new(first_name: 'Nicky', last_name: 'Nicholson', phone_number: '222-2222', email: 'nicky@nicholson.com')
+# contact.save
+
+# contact = Contact.find_by(id: 2)
+# contact.update(phone_number: '111-1111')
+# contact = Contact.find_by(id: 3)
+# contact.update(phone_number: '222-2222')
+# contact = Contact.find_by(id: 4)
+# contact.update(phone_number: '333-3333')
