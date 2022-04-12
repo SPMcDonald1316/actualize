2e61987961dcfe2f37f535b57880125c31a6db1f
sean <spmcdonald1316@gmail.com>
2022-04-12 11:07:24 -0500

    add faker gem and create 10 new contacts in seeds file

--- ruby_rails/contacts_app/db/seeds.rb
+++ ruby_rails/contacts_app/db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # contact = Contact.new(first_name: 'Nicky', last_name: 'Nicholson', phone_number: '222-2222', email: 'nicky@nicholson.com')
 # contact.save
 
@@ -7,3 +8,9 @@
 # contact.update(phone_number: '222-2222')
 # contact = Contact.find_by(id: 4)
 # contact.update(phone_number: '333-3333')
+
+10.times do
+  firstName = Faker::Name.unique.first_name
+  lastName = Faker::Name.unique.last_name
+  Contact.create(first_name: firstName, last_name: lastName, phone_number: Faker::PhoneNumber.cell_phone, email: "#{firstName}@#{lastName}")
+end
