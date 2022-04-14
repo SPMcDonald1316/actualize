67ee7ba0cf8f98d75bbac4ddbf8f1a6f957c3074
sean <spmcdonald1316@gmail.com>
2022-04-14 11:33:41 -0500

    create index route

--- ruby_rails/mini_capstone/app/controllers/api/products_controller.rb
+++ ruby_rails/mini_capstone/app/controllers/api/products_controller.rb
@@ -1,2 +1,5 @@
 class Api::ProductsController < ApplicationController
+  def index
+    
+  end
 end
--- ruby_rails/mini_capstone/config/routes.rb
+++ ruby_rails/mini_capstone/config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+
+  namespace :api do
+    get "/products" => "products#index"
+  end
 end
