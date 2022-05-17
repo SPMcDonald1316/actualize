61880611de9cc807dfe52b69eb1a1704bf68a65c
sean <spmcdonald1316@gmail.com>
2022-05-17 12:13:26 -0500

    remove unused actions and update render syntax

--- ruby_rails/mini_capstone/app/controllers/api/products_controller.rb
+++ ruby_rails/mini_capstone/app/controllers/api/products_controller.rb
@@ -1,21 +1,11 @@
 class Api::ProductsController < ApplicationController
   def index
     @products = Product.all
-    render 'products.json.jb'
+    render 'api/products/index'
   end
 
   def product_one
     @product = Product.find_by(id:1)
-    render 'product.json.jb'
-  end
-
-  def product_two
-    @product = Product.find_by(id:2)
-    render 'product.json.jb'
-  end
-
-  def product_three
-    @product = Product.find_by(id:3)
-    render 'product.json.jb'
+    render 'api/products/product'
   end
 end
