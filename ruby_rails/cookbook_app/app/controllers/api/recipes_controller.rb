a36c958b53164859c19d9527665ac1d366889949
sean <spmcdonald1316@gmail.com>
2022-05-19 11:39:37 -0500

    add show method to recipes controller

--- ruby_rails/cookbook_app/app/controllers/api/recipes_controller.rb
+++ ruby_rails/cookbook_app/app/controllers/api/recipes_controller.rb
@@ -3,4 +3,9 @@ class Api::RecipesController < ApplicationController
     @recipes = Recipe.all
     render 'api/recipes/index'
   end
+
+  def show
+    @recipe = Recipe.find_by(id: params[:id])
+    render 'api/recipes/show'
+  end
 end
