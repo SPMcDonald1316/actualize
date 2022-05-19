e15ce38c90ab0975a88b2d63181eaea8706e5274
sean <spmcdonald1316@gmail.com>
2022-05-19 11:46:52 -0500

    add post route and create method

--- ruby_rails/cookbook_app/app/controllers/api/recipes_controller.rb
+++ ruby_rails/cookbook_app/app/controllers/api/recipes_controller.rb
@@ -8,4 +8,17 @@ class Api::RecipesController < ApplicationController
     @recipe = Recipe.find_by(id: params[:id])
     render 'api/recipes/show'
   end
+
+  def create
+    @recipe = Recipe.new(
+      title: params[:title],
+      ingredients: params[:ingredients],
+      chef: params[:chef],
+      directions: params[:directions],
+      prep_time: params[:prep_time],
+      image_url: params[:image_url]
+    )
+    @recipe.save
+    render 'api/recipes/show'
+  end
 end
--- ruby_rails/cookbook_app/config/routes.rb
+++ ruby_rails/cookbook_app/config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   namespace :api do
     get "/recipes" => "recipes#index"
     get "/recipes/:id" => "recipes#show"
+    post "/recipes" => "recipes#create"
   end
 end
