c99401ab357405893ba9357b90c5454241147088
sean <spmcdonald1316@gmail.com>
2022-05-19 11:33:07 -0500

    clear incorrectly named routes/methods/views

--- ruby_rails/cookbook_app/app/controllers/api/recipes_controller.rb
+++ ruby_rails/cookbook_app/app/controllers/api/recipes_controller.rb
@@ -1,6 +1,2 @@
 class Api::RecipesController < ApplicationController
-  def recipe
-    @recipe = Recipe.first
-    render 'recipes.json.jb'
-  end
 end
--- ruby_rails/cookbook_app/config/routes.rb
+++ ruby_rails/cookbook_app/config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/recipe" => "recipes#recipe"
   end
 end
