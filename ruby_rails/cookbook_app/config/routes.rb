c9a70580dc650cff163a1f8394a9bbc79e4a3a67
sean <spmcdonald1316@gmail.com>
2022-05-20 11:58:50 -0500

    add route for update action

--- ruby_rails/cookbook_app/config/routes.rb
+++ ruby_rails/cookbook_app/config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     get "/recipes" => "recipes#index"
     get "/recipes/:id" => "recipes#show"
     post "/recipes" => "recipes#create"
+    patch "/recipes/:id" => "recipes#update"
   end
 end
