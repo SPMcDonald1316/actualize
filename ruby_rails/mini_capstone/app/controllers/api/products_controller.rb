19b81914a5bf5fd87f137d97a91104d864acbfd0
sean <spmcdonald1316@gmail.com>
2022-05-21 11:39:26 -0500

    create show method

--- ruby_rails/mini_capstone/app/controllers/api/products_controller.rb
+++ ruby_rails/mini_capstone/app/controllers/api/products_controller.rb
@@ -3,4 +3,9 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render 'api/products/index'
   end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render 'api/products/show'
+  end
 end
