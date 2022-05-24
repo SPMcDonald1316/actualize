e612e60b5ee8bbc622e9cdec452bc058fe09994e
sean <spmcdonald1316@gmail.com>
2022-05-24 12:22:21 -0500

    learn more uses for model methods

--- ruby_rails/cookbook_app/app/models/recipe.rb
+++ ruby_rails/cookbook_app/app/models/recipe.rb
@@ -6,4 +6,14 @@ class Recipe < ApplicationRecord
   def directions_list
     directions.split(", ")
   end
+
+  def friendly_created_at
+    created_at.strftime("%m/%d/%Y")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    "#{hours} hours and #{minutes} minutes"
+  end
 end
