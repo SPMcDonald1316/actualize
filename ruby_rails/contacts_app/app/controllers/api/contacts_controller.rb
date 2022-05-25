923a41fd7cd91f8f96c59800782dabe55bca62ad
sean <spmcdonald1316@gmail.com>
2022-05-25 11:36:42 -0500

    clean up routes/controller/views & fix logic for show action

--- ruby_rails/contacts_app/app/controllers/api/contacts_controller.rb
+++ ruby_rails/contacts_app/app/controllers/api/contacts_controller.rb
@@ -1,12 +1,12 @@
 class Api::ContactsController < ApplicationController
   def index
     @contacts = Contact.all
-    render 'index.json.jb'
+    render 'api/contacts/index'
   end
-  
+
   def show
-    @contact = Contact.first
-    render 'contact.json.jb'
+    @contact = Contact.find_by(id: params[:id])
+    render 'api/contacts/show'
   end
 
 end
--- ruby_rails/contacts_app/config/routes.rb
+++ ruby_rails/contacts_app/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # end
 
   namespace :api do
-    get "/contact" => "contacts#show"
     get "/contacts" => "contacts#index"
+    get "/contacts/:id" => "contacts#show"
   end
 end
