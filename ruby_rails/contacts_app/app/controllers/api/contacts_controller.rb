407f57d66e9458d7a7775501fa0de66048d4c4cc
sean <spmcdonald1316@gmail.com>
2022-05-25 11:43:00 -0500

    write route/method for create action

--- ruby_rails/contacts_app/app/controllers/api/contacts_controller.rb
+++ ruby_rails/contacts_app/app/controllers/api/contacts_controller.rb
@@ -9,4 +9,14 @@ class Api::ContactsController < ApplicationController
     render 'api/contacts/show'
   end
 
+  def create
+    @contact = Contact.new({
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    })
+    @contact.save
+    render 'api/contacts/show'
+  end
 end
--- ruby_rails/contacts_app/config/routes.rb
+++ ruby_rails/contacts_app/config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   namespace :api do
     get "/contacts" => "contacts#index"
     get "/contacts/:id" => "contacts#show"
+    post "/contacts" => "contacts#create"
   end
 end
