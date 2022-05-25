a125bc5b4360af94b00df9688ce3208de54d2829
sean <spmcdonald1316@gmail.com>
2022-05-25 12:08:27 -0500

    write model methods to format created and updated time, add methods to views

--- ruby_rails/contacts_app/app/models/contact.rb
+++ ruby_rails/contacts_app/app/models/contact.rb
@@ -1,2 +1,9 @@
 class Contact < ApplicationRecord
+  def friendly_created_at
+    created_at.strftime("%m, %d, %Y")
+  end
+
+  def friendly_updated_at
+    updated_at.strftime("%m, %d, %Y")
+  end
 end
