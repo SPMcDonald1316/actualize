0ea000bbe63c3a6efe3b9c9f637bd3c9e7aaa41e
sean <spmcdonald1316@gmail.com>
2022-05-25 11:27:34 -0500

    rearrange controller methods

--- ruby_rails/contacts_app/app/controllers/api/contacts_controller.rb
+++ ruby_rails/contacts_app/app/controllers/api/contacts_controller.rb
@@ -1,11 +1,12 @@
 class Api::ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+    render 'index.json.jb'
+  end
+  
   def show
     @contact = Contact.first
     render 'contact.json.jb'
   end
 
-  def index
-    @contacts = Contact.all
-    render 'index.json.jb'
-  end
 end
