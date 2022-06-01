6871863876a8730d032f5eefb4d29c1d3d32aacd
sean <spmcdonald1316@gmail.com>
2022-06-01 15:49:48 -0500

    add stock column to products table using shorthand rails generate command

--- ruby_rails/migrations_app/db/schema.rb
+++ ruby_rails/migrations_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_27_163059) do
+ActiveRecord::Schema.define(version: 2022_06_01_204752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_05_27_163059) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "price", precision: 6, scale: 2
+    t.integer "stock", default: 0
   end
 
 end
