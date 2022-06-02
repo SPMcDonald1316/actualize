dec4b8eaa81e56cc3d34aa1a550f2a553725d45a
sean <spmcdonald1316@gmail.com>
2022-06-02 12:54:31 -0500

    add validations to model

--- ruby_rails/mini_capstone/app/models/product.rb
+++ ruby_rails/mini_capstone/app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ApplicationRecord
+  validates :name, :price, presence: true
+  validates :price, numericality: {in: 1..100}
+  validates :name, length: {maximum: 40}
+  validates :description, {in: 10..500}
+
   TAX_RATE = 0.09
+  
   def is_discounted?
     price < 10
   end
