9b7209383a03cf18d0097992f49f7859f9b4856f
sean <spmcdonald1316@gmail.com>
2022-06-14 16:53:54 -0500

    write logic in seed file to create images and apply product ids

--- ruby_rails/mini_capstone/db/seeds.rb
+++ ruby_rails/mini_capstone/db/seeds.rb
@@ -20,8 +20,26 @@ require 'faker'
 #   number += 1
 # end
 
-@products = Product.all
-@products.each do |product|
-  product.supplier_id = rand(1..5)
-  product.save
+# @products = Product.all
+# @products.each do |product|
+#   product.supplier_id = rand(1..5)
+#   product.save
+# end
+
+product_ids = []
+i = 1
+
+while i <= 38
+  if i == 8
+    i += 1
+  end
+  product_ids << i
+  i += 1
+end
+
+100.times do 
+  Image.create(
+    url: 'a photo',
+    product_id: product_ids.sample
+  )
 end
