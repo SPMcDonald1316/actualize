8c997e4d6426c19797eb1a824a362b4f835bc9bd
sean <spmcdonald1316@gmail.com>
2022-06-15 16:53:41 -0500

    write route to create users, generate users controller

--- ruby_rails/cookbook_app/config/routes.rb
+++ ruby_rails/cookbook_app/config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
     post "/recipes" => "recipes#create"
     patch "/recipes/:id" => "recipes#update"
     delete "/recipes/:id" => "recipes#destroy"
+
+    post "/users" => "users#create"
   end
 end
