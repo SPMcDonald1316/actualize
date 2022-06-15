91aff6ea0f3696d28911cfdc3dfbc0065a3dc95f
sean <spmcdonald1316@gmail.com>
2022-06-15 16:48:11 -0500

    create user table

--- ruby_rails/cookbook_app/db/schema.rb
+++ ruby_rails/cookbook_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_16_163741) do
+ActiveRecord::Schema.define(version: 2022_06_15_214720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2022_03_16_163741) do
     t.string "directions"
     t.integer "prep_time"
     t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
