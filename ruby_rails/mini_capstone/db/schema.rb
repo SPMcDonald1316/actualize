d8a814c66ff363a1477ba7383bc251291ee9e863
sean <spmcdonald1316@gmail.com>
2022-06-16 15:41:18 -0500

    create users table

--- ruby_rails/mini_capstone/db/schema.rb
+++ ruby_rails/mini_capstone/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_14_213316) do
+ActiveRecord::Schema.define(version: 2022_06_16_204024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 2022_06_14_213316) do
     t.string "name"
     t.string "email"
     t.string "phone_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
