90990073bfd8bb975db2c5bfd690ba1c4f9b5380
sean <spmcdonald1316@gmail.com>
2022-06-16 15:49:07 -0500

    create user route, create action, and set up password encryption

--- ruby_rails/mini_capstone/config/routes.rb
+++ ruby_rails/mini_capstone/config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
     post "/products" => "products#create"
     patch "/products/:id" => "products#update"
     delete "/products/:id" => "products#destroy"
+
+    post "/users" => "users#create"
   end
 end
