cb06b99fd80f025dd6158e8c1b201359d8c2c782
sean <spmcdonald1316@gmail.com>
2022-06-29 15:32:24 -0500

    fixed current user method

--- ruby_rails/cookbook_app/app/controllers/application_controller.rb
+++ ruby_rails/cookbook_app/app/controllers/application_controller.rb
@@ -3,12 +3,11 @@ class ApplicationController < ActionController::Base
 
   def current_user
     auth_headers = request.headers["Authorization"]
-    if auth_headers.present? && auth_headers[/[?<=\A(Bearer )]\S+\z/]
-      token = auth_headers[/[?<=\A(Bearer )]\S+\z/]
+    if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
       begin
         decoded_token = JWT.decode(
           token,
-          Rails.application.credentials.fetch[:secret_key_base],
+          Rails.application.credentials.fetch(:secret_key_base),
           true,
           { algorithm: "HS256" }
         )
