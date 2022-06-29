d6308b94642889c185efe1da9ef1d32bf31bc71d
sean <spmcdonald1316@gmail.com>
2022-06-29 16:44:53 -0500

    create users table, write users route/ create method/ add validations to user model

--- ruby_rails/contacts_app/config/routes.rb
+++ ruby_rails/contacts_app/config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
     post "/contacts" => "contacts#create"
     patch "/contacts/:id" => "contacts#update"
     delete "/contacts/:id" => "contacts#destroy"
+
+    post "/users" => "users#create"
   end
 end
--- ruby_rails/contacts_app/db/schema.rb
+++ ruby_rails/contacts_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_02_005713) do
+ActiveRecord::Schema.define(version: 2022_06_29_213857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2022_06_02_005713) do
     t.decimal "lat", precision: 8, scale: 5
     t.decimal "long", precision: 8, scale: 5
     t.string "address"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
