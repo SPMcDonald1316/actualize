831474bf65968e4eaaa1c4afcaf4a30d69c3140b
sean <spmcdonald1316@gmail.com>
2022-08-02 16:40:01 -0500

    seed file to add user_ids to contacts

--- ruby_rails/contacts_app/config/routes.rb
+++ ruby_rails/contacts_app/config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
-
   namespace :api do
     get "/contacts" => "contacts#index"
     get "/contacts/:id" => "contacts#show"
--- ruby_rails/contacts_app/db/seeds.rb
+++ ruby_rails/contacts_app/db/seeds.rb
@@ -15,11 +15,40 @@ require 'faker'
 #   Contact.create(first_name: firstName, last_name: lastName, phone_number: Faker::PhoneNumber.cell_phone, email: "#{firstName}@#{lastName}")
 # end
 
-contacts = Contact.all
+used_ids = []
 
-contacts.each do |contact|
-  contact.update({
-    middle_name: Faker::Name.middle_name,
-    bio: Faker::Job.title
-  })
-end
+user_id = 1
+
+con_id = rand(1..14)
+contact = Contact.find_by(id: con_id)
+puts contact.user_id
+
+
+# while used_ids.length < 3
+#   con_id = rand(1..14)
+#   if !used_ids.include?(con_id)
+#     used_ids.append(con_id)
+#     contact = Contact.find_by(id: con_id)
+#     contact.user_id = user_id
+#   end
+# end
+
+# user_id = 2
+# while used_ids.length < 6
+#   con_id = rand(1..14)
+#   if !used_ids.include?(con_id)
+#     used_ids.append(con_id)
+#     contact = Contact.find_by(id: con_id)
+#     contact.user_id = user_id
+#   end
+# end
+
+# user_id = 3
+# # while used_ids.length < 9
+# #   con_id = rand(1..14)
+# #   used_ids.append(con_id)
+# #   if !used_ids.include?(con_id)
+# #     contact = Contact.find_by(id: con_id)
+# #     contact.user_id = user_id
+# #   end
+# # end
