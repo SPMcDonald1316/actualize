958e8c056a19f7e90544398c7679158aadb5975b
sean <spmcdonald1316@gmail.com>
2022-08-05 15:26:02 -0500

    initial attempt for create action

--- ruby_rails/mini_capstone/app/controllers/api/orders_controller.rb
+++ ruby_rails/mini_capstone/app/controllers/api/orders_controller.rb
@@ -1,2 +1,18 @@
 class Api::OrdersController < ApplicationController
+  def create
+    product = Product.find_by(id: params[:product_id])
+    TAX_RATE = .10
+    @order = Order.new({
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: product.price * params[:quantity],
+      tax: subtotal * TAX_RATE,
+      total: subtotal + tax
+    })
+    if @order.save
+      render 'api/orders/show'
+    else
+      render {errors: @order.errors.full_messages}, status: :unprocessable_entity
+  end
 end
