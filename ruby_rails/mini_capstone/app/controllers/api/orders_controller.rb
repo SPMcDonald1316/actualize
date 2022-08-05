6e62594ed50e1a4cfb74eea20c0d2ca49daf3a86
sean <spmcdonald1316@gmail.com>
2022-08-05 15:47:00 -0500

    fix errors in order create

--- ruby_rails/mini_capstone/app/controllers/api/orders_controller.rb
+++ ruby_rails/mini_capstone/app/controllers/api/orders_controller.rb
@@ -1,15 +1,16 @@
 class Api::OrdersController < ApplicationController
   def create
     product = Product.find_by(id: params[:product_id])
+    tax_rate = 0.10
     @order = Order.new({
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: product.price * params[:quantity],
-      tax: subtotal * 0.10,
-      total: subtotal + tax
+      subtotal: product.price * params[:quantity].to_i,
     })
     if @order.save
+      @order.tax = @order.subtotal * tax_rate
+      @order.total = @order.subtotal + @order.tax
       render 'api/orders/show'
     else
       render json: {errors: @order.errors.full_messages}, status: :unprocessable_entity
