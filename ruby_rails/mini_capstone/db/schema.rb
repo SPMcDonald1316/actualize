6c0c8ea1baf90e97a0cdf69a828848f986c40ca7
sean <spmcdonald1316@gmail.com>
2022-08-09 16:21:33 -0500

    create categories and category_products tables

--- ruby_rails/mini_capstone/db/schema.rb
+++ ruby_rails/mini_capstone/db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_09_204407) do
+ActiveRecord::Schema.define(version: 2022_08_09_211308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "category_products", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "product_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.string "url"
