d84d33219ab0ca16a73b7d2e2846d6cc4d3cb0b0
sean <spmcdonald1316@gmail.com>
2022-08-13 12:45:07 -0500

    create carted_products controller

--- ruby_rails/mini_capstone/app/controllers/api/carted_products_controller.rb
+++ ruby_rails/mini_capstone/app/controllers/api/carted_products_controller.rb
@@ -1,2 +1,14 @@
 class Api::CartedProductsController < ApplicationController
+  def create
+    if current_user
+      carted_product = CartedProduct.new({
+        status: 'carted',
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity]
+      })
+    else
+      render json: {error: "Please login first."}, status: :unprocessible_entity
+    end
+  end
 end
