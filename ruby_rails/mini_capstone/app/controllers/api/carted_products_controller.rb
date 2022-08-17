9c22f8737156c3b9894342f51713bbf6ca71fbe3
sean <spmcdonald1316@gmail.com>
2022-08-17 15:37:11 -0500

    use current user information for putting items in cart

--- ruby_rails/mini_capstone/app/controllers/api/carted_products_controller.rb
+++ ruby_rails/mini_capstone/app/controllers/api/carted_products_controller.rb
@@ -1,5 +1,6 @@
 class Api::CartedProductsController < ApplicationController
   def create
+    if current_user
       @carted_product = CartedProduct.new({
         user_id: current_user.id,
         product_id: params[:product_id],
@@ -8,5 +9,8 @@ class Api::CartedProductsController < ApplicationController
       })
       @carted_product.save
       render 'api/carted_products/show'
+    else
+      render json: {message: 'Please log in first.'}, status: :unauthorized 
+    end
   end
 end
