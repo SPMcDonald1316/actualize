4ec3710ea64f08d27b8a306189f49f12232852de
sean <spmcdonald1316@gmail.com>
2022-08-22 15:27:15 -0500

    index route using third party api / and learn to hide api key

--- ruby_rails/news_api/app/controllers/api/articles_controller.rb
+++ ruby_rails/news_api/app/controllers/api/articles_controller.rb
@@ -1,6 +1,7 @@
 class Api::ArticlesController < ApplicationController
   def index
-    
+    response = HTTP.get("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=#{Rails.application.credentials.news_api[:api_key]}")
+    @articles = response.parse
     render "api/articles/index"
   end
 end
