2b6e9caee92a7a3c0d3f7fe31817df010b082a71
sean <spmcdonald1316@gmail.com>
2022-08-26 15:56:25 -0500

    vue index action

--- javascript_vue/simple_mini_capstone_vue/src/views/HomeView.vue
+++ javascript_vue/simple_mini_capstone_vue/src/views/HomeView.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <h1>{{ name }}</h1>
+    <!-- <h1>{{ products }}</h1> -->
+    <div v-for="product in products">
+      <p>{{ product.id }}. {{ product.name }}</p>
+      <p>{{ product.description }}</p>
+      <p>{{ product.formatted.price }}</p>
+    </div>
   </div>
 </template>
 
@@ -8,13 +15,20 @@
 </style>
 
 <script>
+ import axios from "axios";
   export default {
     data: function() {
       return {
-        message: "Welcome to Vue.js!"
+        message: "Welcome to Vue.js!",
+        name: "sean",
+        products: []
       };
     },
-    created: function() {},
+    created: function() {
+      axios.get("/products").then(response => {
+        this.products = response.data;
+      })
+    },
     methods: {}
   };
 </script>
