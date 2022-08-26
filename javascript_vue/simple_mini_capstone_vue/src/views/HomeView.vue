c8074978f0f3caf7f81ee4188eab7e3aaeedfcd1
sean <spmcdonald1316@gmail.com>
2022-08-26 16:20:54 -0500

    begin vue new action

--- javascript_vue/simple_mini_capstone_vue/src/views/HomeView.vue
+++ javascript_vue/simple_mini_capstone_vue/src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <h1>{{ message }}</h1>
     <h1>{{ name }}</h1>
     <!-- <h1>{{ products }}</h1> -->
+    <button v-on:click="addProduct()">Add A New Product</button>
     <div v-for="product in products">
       <p>{{ product.id }}. {{ product.name }}</p>
       <p>{{ product.description }}</p>
@@ -31,6 +32,18 @@
         this.products = response.data;
       })
     },
-    methods: {}
+    methods: {
+      addProduct: function() {
+        var params = {
+          name: 'baseball bat',
+          price: 65.99,
+          description: 'For hitting dingers!',
+          image_url: "https://tse1.mm.bing.net/th?id=OIP.3WSHRT2Le2IY6f0bCAEP2wHaHa&pid=Api"
+        }
+        axios.post("/products", params).then(response => {
+          console.log(response.data);
+        })
+      }
+    }
   };
 </script>
