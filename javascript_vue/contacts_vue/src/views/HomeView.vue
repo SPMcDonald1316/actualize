4662180c0301de276beca870c4b5b675860e6ea6
sean <spmcdonald1316@gmail.com>
2022-09-06 16:34:28 -0500

    frontend index action

--- javascript_vue/contacts_vue/src/views/HomeView.vue
+++ javascript_vue/contacts_vue/src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="home">
-    
+    <h1>Contacts</h1>
+    <div v-for="contact in contacts">
+      <p>Name: {{contact.name}}</p>
+      <p>Number: {{contact.phone}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-  
+  import axios from "axios";
+  export default {
+    data: function() {
+      return {
+        contacts: []
+      };
+    },
+    created: function() {
+      axios.get("/api/contacts").then(response => {
+        this.contacts = response.data;
+      });
+    },
+    methods: {}
+  };
 </script>
