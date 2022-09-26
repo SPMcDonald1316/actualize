2bfa51b30012b4afade86c6fc1e21d869d056044
sean <spmcdonald1316@gmail.com>
2022-09-26 16:06:43 -0500

    start login component

---
+++ javascript_vue/cookbook_vue/src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login">
+    <form v-on:submit.prevent="submit()">
+      <h1>
+        <ul>
+          <li class="text-danger" v-for="error in errors">
+            {{ errors }}
+          </li>
+        </ul>
+      </h1>
+    </form>
+  </div>
+</template>
