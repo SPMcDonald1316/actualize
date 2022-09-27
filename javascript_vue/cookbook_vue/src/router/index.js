fe8daa8a293d1745a8ad806afaae9f0c95ddfc0f
sean <spmcdonald1316@gmail.com>
2022-09-27 15:14:45 -0500

    create login route

--- javascript_vue/cookbook_vue/src/router/index.js
+++ javascript_vue/cookbook_vue/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Signup from '../views/Signup.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,11 @@ const routes = [
     path: '/signup',
     name: 'signup',
     component: Signup
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   }
 ]
 
